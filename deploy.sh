6ebe594970caf941f6c760e14dd1576e5e1930ea
NasirovEF <i@ef-nasirov.ru>
2024-09-06 00:16:35 +0300

    add coursework 8

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#python3 -m venv venv
+#source env/bin/activate
+#pip3 install -r requirements.txt
+#python3 manage.py migrate
+#python3 manage.py collectstatic --no-input
+
+docker-compose up --build -d
+docker-compose exec app python3 manage.py migrate
+#docker-compose exec app python manage.py createsuperuser
